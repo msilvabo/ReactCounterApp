bccc6cfed31ea6efc4de0e903f4da040956f2364
Moises Silva Cardenas <silva.moises@gmail.com>
2025-03-10 13:19:19 -0400

    Seccion 4 - Nuestro primer componente

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import HelloApp from './HelloApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HelloApp/> 
+    </React.StrictMode>
+);
